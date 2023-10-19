ac468d6dce438425920c8c713e8ddaccebd653de
LEGENDARY1209 <130669909+LEGENDARY1209@users.noreply.github.com>
2023-10-19 11:58:48 -0300

    commit

---
+++ métododest.php
@@ -0,0 +1,28 @@
+<?php
+class Car
+{
+        public $color = 'red';
+        public $numberOfTires = 4;
+        public function _construct($color)
+        {
+            $this->color = $color;
+        }
+         Public function __destruct()
+        {
+               echo "Objeto está sendo destruído";
+        }
+        public function setColor($val)
+        {
+                $this->color = $val;
+        }
+        public function getColor()
+        {
+            return $this->color;
+        }
+}
+
+$bmw = new Car("white");
+echo $bmw->getColor(); //isso imprimirá "branco" porque nosso construtor atribuiu o valor "branco" à propriedade color
+echo $bmw->setColor("black"); // isso irá sobrescrever a propriedade da cor para preto  
+echo $bmw->getColor();
+?>
